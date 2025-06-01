4838b367e790ea3d3064acbd9e93d38b97602e31
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-06-01 23:03:49 +0200

    uh

--- test/unit/blob-mode-reducer.test.js
+++ test/unit/blob-mode-reducer.test.js
@@ -15,7 +15,7 @@ test('initialState', () => {
 test('changeBrushSize', () => {
     let defaultState;
     const newBrushSize = 8078;
-    const initialSegmentSize = 1;
+    const initialSegmentSize = 10;
 
     expect(brushReducer(defaultState /* state */, changeBrushSize(newBrushSize) /* action */))
         .toEqual({brushSize: newBrushSize, segSize: initialSegmentSize});
