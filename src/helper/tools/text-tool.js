d2a6aaf38ffc062d10fa386b9920cdc45a3bfa83
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-07-18 17:35:41 +0200

    oops

--- src/helper/tools/text-tool.js
+++ src/helper/tools/text-tool.js
@@ -343,7 +343,7 @@ class TextTool extends paper.Tool {
                 // is 120%, but for some reason they are slightly off from each other.
                 // This value was obtained experimentally.
                 leading: 46.15,
-                textDecoration = 'none'
+                textDecoration: 'none'
             });
             this.beginTextEdit(this.textBox);
         }
