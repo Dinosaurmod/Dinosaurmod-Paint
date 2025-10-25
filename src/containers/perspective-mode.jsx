81674d0e37c16184faeef4b6f7aecc091632bc1c
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-10-25 21:16:44 +0200

    uh

--- src/containers/perspective-mode.jsx
+++ src/containers/perspective-mode.jsx
@@ -8,11 +8,67 @@ import ColorStyleProptype from '../lib/color-style-proptype';
 import {changeMode} from '../reducers/modes';
 import {MIXED} from '../helper/style-path';
 
+import {changeMode} from '../reducers/modes';
+import {clearHoveredItem, setHoveredItem} from '../reducers/hover';
+import {clearSelectedItems, setSelectedItems} from '../reducers/selected-items';
+import {setCursor} from '../reducers/cursor';
+
 import {clearSelection, getSelectedLeafItems} from '../helper/selection';
-import PerspectiveTool from '../helper/tools/perspective-tool.js';
+import PerspectiveTool from '../helper/selection-tools/perspective-tool.js';
 import PerspectiveModeComponent from '../components/perspective-mode/perspective-mode.jsx';
 
 class PerspectiveMode extends React.Component {
+    constructor (props) {
+        super(props);
+        bindAll(this, [
+            'activateTool',
+            'deactivateTool'
+        ]);
+    }
+    componentDidMount () {
+        if (this.props.isPerspectiveModeActive) {
+            this.activateTool(this.props);
+        }
+    }
+    componentWillReceiveProps (nextProps) {
+        if (this.tool && nextProps.hoveredItemId !== this.props.hoveredItemId) {
+            this.tool.setPrevHoveredItemId(nextProps.hoveredItemId);
+        }
+        if (this.tool && nextProps.selectedItems !== this.props.selectedItems) {
+            this.tool.onSelectionChanged(nextProps.selectedItems);
+        }
+
+        if (nextProps.isPerspectiveModeActive && !this.props.isPerspectiveModeActive) {
+            this.activateTool();
+        } else if (!nextProps.isPerspectiveModeActive && this.props.isPerspectiveModeActive) {
+            this.deactivateTool();
+        }
+    }
+    shouldComponentUpdate (nextProps) {
+        return nextProps.isPerspectiveModeActive !== this.props.isPerspectiveModeActive;
+    }
+    componentWillUnmount () {
+        if (this.tool) {
+            this.deactivateTool();
+        }
+    }
+    activateTool () {
+        this.tool = new PerspectiveTool(
+            this.props.setHoveredItem,
+            this.props.clearHoveredItem,
+            this.props.setSelectedItems,
+            this.props.clearSelectedItems,
+            this.props.setCursor,
+            this.props.onUpdateImage,
+            this.props.switchToTextTool
+        );
+        this.tool.activate();
+    }
+    deactivateTool () {
+        this.tool.deactivateTool();
+        this.tool.remove();
+        this.tool = null;
+    }
     render () {
         return (
             <PerspectiveModeComponent
@@ -24,18 +80,45 @@ class PerspectiveMode extends React.Component {
 }
 
 PerspectiveMode.propTypes = {
+    clearHoveredItem: PropTypes.func.isRequired,
+    clearSelectedItems: PropTypes.func.isRequired,
     handleMouseDown: PropTypes.func.isRequired,
+    hoveredItemId: PropTypes.number,
     isPerspectiveModeActive: PropTypes.bool.isRequired,
+    onUpdateImage: PropTypes.func.isRequired,
     selectedItems: PropTypes.arrayOf(PropTypes.instanceOf(paper.Item)),
-    setSelectedItems: PropTypes.func.isRequired
+    setCursor: PropTypes.func.isRequired,
+    setHoveredItem: PropTypes.func.isRequired,
+    setSelectedItems: PropTypes.func.isRequired,
+    switchToTextTool: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-    isPerspectiveModeActive: state.scratchPaint.mode === Modes.PERSPECTIVE
+    isPerspectiveModeActive: state.scratchPaint.mode === Modes.PERSPECTIVE,
+    hoveredItemId: state.scratchPaint.hoveredItemId,
+    selectedItems: state.scratchPaint.selectedItems
 });
 const mapDispatchToProps = dispatch => ({
+    setHoveredItem: hoveredItemId => {
+        dispatch(setHoveredItem(hoveredItemId));
+    },
+    clearHoveredItem: () => {
+        dispatch(clearHoveredItem());
+    },
+    clearSelectedItems: () => {
+        dispatch(clearSelectedItems());
+    },
+    setSelectedItems: () => {
+        dispatch(setSelectedItems(getSelectedLeafItems(), false /* bitmapMode */));
+    },
+    setCursor: cursorString => {
+        dispatch(setCursor(cursorString));
+    },
     handleMouseDown: () => {
         dispatch(changeMode(Modes.PERSPECTIVE));
+    },
+    switchToTextTool: () => {
+        dispatch(changeMode(Modes.TEXT));
     }
 });
 
