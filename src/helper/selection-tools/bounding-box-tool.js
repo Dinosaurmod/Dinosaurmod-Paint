47d532fb5f49f78dc611bcad74491af2aac05c3f
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-11-16 13:30:36 +0100

    A

--- src/helper/selection-tools/bounding-box-tool.js
+++ src/helper/selection-tools/bounding-box-tool.js
@@ -50,8 +50,8 @@ class BoundingBoxTool {
         this.boundsRotHandles = [];
         this._modeMap = {};
         this._modeMap[BoundingBoxModes.SCALE] = new ScaleTool(mode, onUpdateImage, isPerspective);
-        if (!isPerspective) this._modeMap[BoundingBoxModes.ROTATE] = new RotateTool(onUpdateImage);
-        if (!isPerspective) this._modeMap[BoundingBoxModes.MOVE] = new MoveTool(mode, setSelectedItems, clearSelectedItems, onUpdateImage, switchToTextTool);
+        this._modeMap[BoundingBoxModes.ROTATE] = new RotateTool(onUpdateImage);
+        this._modeMap[BoundingBoxModes.MOVE] = new MoveTool(mode, setSelectedItems, clearSelectedItems, onUpdateImage, switchToTextTool);
         this._currentCursor = null;
     }
 
